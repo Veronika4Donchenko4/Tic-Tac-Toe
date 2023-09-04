52b2c306bc4d78af08d05bc2d8aab462c019cc1b
Nika Donchenko <donchenko.veronika96@gmail.com>
2023-09-04 23:51:13 +0200

    completed

--- script.js
+++ script.js
@@ -1,7 +1,53 @@
 let userTurn = true;
 let message = document.querySelector('#message');
+let playerXScore = localStorage.getItem('X');
+let playerOScore = localStorage.getItem('O');
+let playerXScoreElement = document.querySelector('#player-x-score');
+let playerOScoreElement = document.querySelector('#player-o-score');
+let randomNumber = Math.random() * 10;
 
-function logic() {
+function checkScore() {
+    /*
+     * When user loads the page for the first time
+     * playerXScore value is null
+     */
+    if (!playerXScore && !playerOScore) {
+        localStorage.setItem('X', 0);
+        localStorage.setItem('O', 0);
+        playerXScore = 0;
+        playerOScore = 0;
+    }
+    playerXScoreElement.textContent = localStorage.getItem('X');
+    playerOScoreElement.textContent = localStorage.getItem('O');
+}
+checkScore();
+
+// Randomize each player's turn
+if (randomNumber < 5.5) {
+    userTurn = true;
+    message.textContent = "Player X's Turn";
+} else {
+    userTurn = false;
+    message.textContent = "Player O's Turn";
+}
+
+// Mark X or O in the selected cell
+function markCell(event) {
+    let input = event.target;
+
+    if (userTurn === true) {
+        input.value = 'X';
+        input.disabled = true;
+        userTurn = false;
+    } else {
+        input.value = 'O';
+        input.disabled = true;
+        userTurn = true;
+    }
+}
+
+// The main logic of winning the game
+function gameLogic() {
     // Array of id names of cells
     let cellIds = [
         'cell-1',
@@ -21,11 +67,7 @@ function logic() {
     );
 
     // Array of cell values
-    let cellValues = cellElements.map(
-        (cell) => cell.value
-    );
-
-    console.log(cellValues);
+    let cellValues = cellElements.map((cell) => cell.value);
 
     // Array of winning patterns
     let winPatterns = [
@@ -39,57 +81,79 @@ function logic() {
         [2, 4, 6],
     ];
 
-    let emptyCells = cellValues.filter(value => value === '');
+    // Flag for match tied condition
+    let won = false;
+
+    // Number of empty cells left
+    let numberOfEmptyCells = cellValues.filter((value) => value === '').length;
 
     // Evaluate if X or O wins
-    winPatterns.forEach(pattern => {
-        if (
-            cellValues[pattern[0]] === 'X' &&
-            cellValues[pattern[1]] === 'X' &&
-            cellValues[pattern[2]] === 'X'
-        ) {
-            message.textContent = 'Player X Wins!';
-            disableInputs();
+    winPatterns.forEach((pattern) => {
+        let cell1 = cellElements[pattern[0]];
+        let cell2 = cellElements[pattern[1]];
+        let cell3 = cellElements[pattern[2]];
+        if (cell1.value === 'X' && cell2.value === 'X' && cell3.value === 'X') {
+            cell1.classList.add('win');
+            cell2.classList.add('win');
+            cell3.classList.add('win');
+            winner('X');
         } else if (
-            cellValues[pattern[0]] === 'O' &&
-            cellValues[pattern[1]] === 'O' &&
-            cellValues[pattern[2]] === 'O'
+            cell1.value === 'O' &&
+            cell2.value === 'O' &&
+            cell3.value === 'O'
         ) {
-            message.textContent = 'Player O Wins!';
-            disableInputs();
-        } else if (emptyCells.length === 0) {
-            message.textContent = 'Match Tied!';
+            cell1.classList.add('win');
+            cell2.classList.add('win');
+            cell3.classList.add('win');
+            winner('O');
         }
     });
 
+    // Check if the match is tied
+    if (numberOfEmptyCells === 0 && won === false) {
+        message.textContent = 'Match Tied!';
+    }
+
+    // Winner player logic
+    function winner(player) {
+        if (player === 'X') {
+            playerXScore = Number(playerXScore) + 1;
+            localStorage.setItem('X', playerXScore);
+            playerXScoreElement.textContent = playerXScore;
+            message.textContent = 'Player X Wins!';
+        } else {
+            playerOScore = Number(playerOScore) + 1;
+            localStorage.setItem('O', playerOScore);
+            playerOScoreElement.textContent = playerOScore;
+            message.textContent = 'Player O Wins!';
+        }
+        message.classList.add('win');
+        disableInputs();
+        won = true;
+    }
+
+    // Disable all inputs
     function disableInputs() {
-        cellElements.forEach(cell => {
+        cellElements.forEach((cell) => {
             cell.disabled = true;
-        })
+        });
     }
 }
 
-function mark(event) {
-    let input = event.target;
-
-    if (userTurn === true) {
-        input.value = 'X';
-        input.disabled = true;
-        userTurn = false;
-    } else {
-        input.value = 'O';
-        input.disabled = true;
-        userTurn = true;
-    }
-}
-
-function reset() {
+// Restart the game
+function restart() {
     window.location.reload();
 }
 
-
-const score = () => {
-    console.log(winPatterns)
+// Reset the score and restart the game
+function reset() {
+    let message = 'Do you really want to RESET the score?';
+    if (confirm(message) === true) {
+        localStorage.setItem('X', 0);
+        localStorage.setItem('O', 0);
+        alert('Score Reset!');
+        restart();
+    } else {
+        alert('Score Kept!');
+    }
 }
-
-score()
